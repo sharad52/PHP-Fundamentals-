f53a4cc39d916cd619846a88f43c609f14e06498
sharad52 <chemist.sharad@gmail.com>
2020-08-25 21:49:58 +0545

    data types in php

---
+++ data_types.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        /*--data types in php --*/
+            //String
+            //Integer
+            //Float(floating point numbers-also called double)
+            //Boolean
+            //Array
+            //Object
+            //Null
+            
+        //string data type
+            
+        $firstName = "Namaste Nepal";
+        //number data type
+        $value = 25;
+        //float
+        $floatValue = 4.35;
+        //Boolean 
+        $isDecimal = true;
+        $isNumber = false;
+        //Array
+        $arr = array();//this is empty array 
+        //Object data types will be discussed in classes and objects
+        //null - this is special type of data types in php which have null value 
+
+        $nullData = null;
+        
+
+
+
+    
+    
+    
+    
+    ?>
+</body>
+</html>
--- variables.php
+++ variables.php
@@ -1,1 +1,39 @@
-<h1>hello world</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Variables </title>
+</head>
+<body>
+    <?php 
+        //variables can store any kind of legal data
+        //in PHP we must give dollar($) sign before variable name 
+        
+        $x = 10; //x variable hold value of 10
+        $name = "Ram Bahadur";//name variable hold the string value which is "Ram Bahadur"
+        
+        $float_number = 4.5;//this holds floating number
+        $gpa = 4.0; //this also holds floating number
+
+        static $PI = 3.141592653589793238;//this static keyword keep this PI variable always same just like const in js if you are familiar with js 
+
+        /*now fetching these variables in html and showing them in browser */
+
+        echo "the value of variable x is:$x";
+        $x++;//this will increase the value of x by 1
+        echo "<br>";
+        echo "$x";
+        //the $studentArray variable holds array 
+        $studentArray = array('ram','shyam','hari','gita','sita',12,14,2.35);
+
+        //we can fetch those array values using index of corresponding values
+
+        echo $studentArray[4];
+
+
+    ?>
+
+
+</body>
+</html>
