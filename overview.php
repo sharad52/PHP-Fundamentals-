ad22540595f789fce9950b00e0e639f25ba80402
sharad52 <chemist.sharad@gmail.com>
2020-08-25 19:49:57 +0545

    variables.php

--- overview.php
+++ overview.php
@@ -1,5 +1,39 @@
 <?php
     //code goes here 
     //this is the syntax to write php 
+    /* Course includes following topics 
+    ->variables 
+    ->data types
+    ->Strings and string method in php
+    ->Numbers and Number methods in php
+    ->Getting User input in php
+    ->Url parameters
+    ->post vs get 
+    ->array  and array methods
+    ->Associative arrays
+    ->Functions and higher order functions in php
+    ->if statements /if---else /nested if 
+    ->Switch Statements
+    ->while loops
+    ->do--while loops
+    ->for loops 
+    ->foreach loops
+    ->comments
+    ->including html
+    ->include php
+    ->classes and objects
+    ->Constructors in php
+    ->Object functions
+    ->Getters and Setters 
+    ->Inheritance
     
+    
+    
+    
+    
+    */
 ?>
+
+
+
+
