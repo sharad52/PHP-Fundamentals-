a785932483f5fad560f7949e98dc574d6cdb32e5
sharad52 <chemist.sharad@gmail.com>
2020-08-28 13:30:37 +0545

    array and methods in array along with foreach loop

--- array.php
+++ array.php
@@ -1,7 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array in php</title>
+</head>
+<body>
+
+    
+
+
+
+
+
+
+
+
+
+
 <?php
+
+include "form.html";
 /*----Arrays---- */
 //very important aspect of any programming language.p.s. in python list are similar things like array
 
+
+// accessing values from html form
+
+$fruitCollection = $_POST["fruits"];
+echo $fruitCollection[2];
+echo "<br>";
 //defining array
 $studentList = array('Diamond','sushant','Divya','Biyani');
 
@@ -19,9 +47,39 @@ for($i=0;$i<count($studentList);$i++){
     echo "$studentList[$i]<br>";
 }
 
-$gpa = array('Biyani'=>4.0,'Sushant'=>3.9);
+//Associative array
+//key-value pair array
 
-print_r($gpa);
+
+$gpa = array('Biyani'=>4.0,'Sushant'=>3.9,'Diamond'=>4.0,'Divya'=>3.88);
+//accessing associative array 
+//need to pass key in array 
+echo $gpa['Biyani'];
+echo "<br/>";
+
+//iteariting associative array 
+//foreach loop 
+
+/* foreach loop syntax:
+
+    foreach($array as $key => $value){
+        //code;
+    }
+*/
+
+$Animals = array('pigeons'=>'Birds',
+        'Tiger'=>'Mammal',
+        'Snake'=>'Reptile',
+        'Frog'=>'Amphibian',
+        'Gadus morhua'=>'pisces');
+
+
+    foreach ($Animals as $name => $class) {
+        echo $name .' '.$class.'<br/>' ;
+    }
 
 
 ?>
+
+</body>
+</html>
